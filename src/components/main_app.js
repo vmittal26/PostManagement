a80abc83c5930a270c12d97c1bf70226eecb42da
vaibhavm <vaibhavmittal.26@gmail.com>
2018-05-29 01:34:25 +0530

    added post form

--- src/components/main_app.js
+++ src/components/main_app.js
@@ -4,9 +4,7 @@ import Posts from './posts';
 export default() => {
     return (
         <main className="mdl-layout__content">
-            <div className="page-content">
                <Posts />
-            </div>
         </main>
     );
 }
--- src/components/posts.js
+++ src/components/posts.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
-import { connect } from 'react-redux';
+import {connect} from 'react-redux';
 import fetchPosts from '../actions/index';
 
 class Posts extends Component {
 
     componentWillMount() {
-        this.props.fetchPosts();
+        this
+            .props
+            .fetchPosts();
     }
     constructor(props) {
         super(props);
@@ -14,9 +16,26 @@ class Posts extends Component {
 
     render() {
         return (
-            <div className="">Loading Posts</div>
+            <div
+                className="demo-card-wide demo-card-square mdl-card mdl-shadow--2dp post-form">
+                <h3 className="post-form__title">Create Posts</h3>
+                <form className="post-form__form">
+                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input className="mdl-textfield__input " type="text" id="sample1"></input>
+                        <label className="mdl-textfield__label ">Title</label>
+                    </div>
+                    <div className="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+                        <input className="mdl-textfield__input " type="text" id="sample1"></input>
+                        <label className="mdl-textfield__label ">Categories</label>
+                    </div>
+                    <div className="mdl-textfield mdl-js-textfield">
+                        <textarea className="mdl-textfield__input" type="text" rows="9"></textarea>
+                        <label className="mdl-textfield__label">Description</label>
+                    </div>
+                </form>
+            </div>
         );
     }
 }
 
-export default connect(null,{fetchPosts})(Posts);
+export default connect(null, {fetchPosts})(Posts);
